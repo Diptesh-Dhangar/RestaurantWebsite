c523755e7e983c871be6bba7d5fe63e236e9da5b
Diptesh-Dhangar <dhangardiptesh2001@gmail.com>
2024-09-08 22:41:15 +0530

    first

---
+++ DBConnection.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(0);
+$host = "localhost"; 
+$username = "C##DSD";
+$password = "DIPTESH";
+
+
+ $conn = connection_aborted($host,$username,$password);
+ echo'connection success';
+
+?>
